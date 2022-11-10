5cd9ccbd25aec990d2f7a64f6829ab1da6f2bed2
mahshidghaffari <m.ghaffari@student.maastrichtuniversity.nl>
2022-11-10 11:34:06 +0100

    initial project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+// import logo from './assets/logo.svg';
+import logo from './assets/logo-p.jpg'
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          yo mahshidddddddd
         </p>
         <a
           className="App-link"
